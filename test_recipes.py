5e1dbb8bfc6591f254499290446010384c3a9048
Прокопенко Данила <danilaprokopenko207@gmail.com>
2026-06-04 02:35:47 +0300

    Исправление ошибок в тестах класса Ingredient: изменено повторное название функции и доработана проверка __str__.

--- test_recipes.py
+++ test_recipes.py
@@ -19,7 +19,7 @@ def test_str_format():
     quantity = 2
     unit = "кг"
     ingredient = Ingredient(name, quantity, unit)
-    assert ingredient == f"{name}: {quantity} {unit}"
+    assert str(ingredient) == f"{name}: {quantity} {unit}"
 
 def test_eq_same_ingredients():
     name1 = "Соль"
@@ -43,7 +43,7 @@ def test_eq_not_same_name():
     ingredient2 = Ingredient(name2, quantity2, unit2)
     assert ingredient1 != ingredient2
 
-def test_eq_same_ingredients():
+def test_eq_not_same_unit():
     name1 = "Соль"
     quantity1 = 2
     unit1 = "кг"
