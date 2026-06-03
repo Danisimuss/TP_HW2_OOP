3abbdf7c08340ef40f9cf6bb5134019c4348b9cd
Прокопенко Данила <danilaprokopenko207@gmail.com>
2026-06-04 01:28:31 +0300

    Добавлен класс Recipe, представляющий собой рецепт одного блюда. Нужен для объединения названия блюда с набором ингредиентов для него, а также предоставить методы для оаботы с этими ингредиентами.

--- main.py
+++ main.py
@@ -27,3 +27,37 @@ class Ingredient:
         if not(isinstance(Ingredient, other)):
             return False
         return self.name == other.name and self.unit == other.unit
+
+
+
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for some_ingredient in self.ingredients:
+            if ingredient == some_ingredient:
+                some_ingredient.quantity += ingredient.quantity
+            else:
+                self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if (type(ratio) == int or type(ratio) == float) and ratio > 0:
+            return True
+        else:
+            return False
+
+    def scale(self, ratio: float):
+        new_ingredients = []
+        for ingred in self.ingredients:
+            new_ingred = Ingredient(ingred.name, ingred.quantity * ratio, ingred.unit)
+            new_ingredients.append(new_ingred)
+        return Recipe(self.title, new_ingredients)
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        return f"Название блюда: {self.title}, список ингредиентов: {' '.join([str(i) for i in self.ingredients])}"
+
