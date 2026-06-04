b05bb3318dcb47f8de3c30a15c76e5eac90e752a
Прокопенко Данила <danilaprokopenko207@gmail.com>
2026-06-04 03:20:01 +0300

    Добавление тестов для класса Recipe и добавление .gitignore

--- test_recipes.py
+++ test_recipes.py
@@ -1,4 +1,6 @@
-from main import Ingredient
+from main import Ingredient, Recipe
+import pytest
+
 def test_init_Ingredient_name():
     name = "Соль"
     ingredient = Ingredient(name, 2, "кг")
@@ -53,3 +55,45 @@ def test_eq_not_same_unit():
     unit2 = "т"
     ingredient2 = Ingredient(name2, quantity2, unit2)
     assert ingredient1 != ingredient2
+
+def test_init_Recipe_title():
+    title = "Шарлотка"
+    recipe = Recipe(title, [Ingredient("Соль", 2, "кг")])
+    assert recipe.title == title
+
+def test_init_Recipe_ingredients():
+    ingredients = [Ingredient("Соль", 2, "кг")]
+    recipe = Recipe("Шарлотка", ingredients)
+    assert recipe.ingredients == [Ingredient("Соль", 2, "кг")]
+
+def test_add_new_ingredient():
+    recipe = Recipe("Шарлотка", [Ingredient("Соль", 2, "кг")])
+    recipe.add_ingredient(Ingredient("Перец", 3, "кг"))
+    assert recipe.ingredients == [Ingredient("Соль", 2, "кг"), Ingredient("Перец", 3, "кг")]
+
+def test_add_same_ingredient():
+    recipe = Recipe("Шарлотка", [Ingredient("Соль", 2, "кг")])
+    recipe.add_ingredient(Ingredient("Соль", 3, "кг"))
+    assert recipe.ingredients[0].quantity == 5
+
+def test_scale_return():
+    recipe1 = Recipe("Шарлотка", [Ingredient("Соль", 2, "кг")])
+    recipe2 = recipe1.scale(3)
+    assert recipe1 != recipe2
+
+def test_scale_work():
+    recipe1 = Recipe("Шарлотка", [Ingredient("Соль", 2, "кг")])
+    recipe2 = recipe1.scale(3)
+    assert recipe2.ingredients[0].quantity == 6
+
+@pytest.mark.parametrize(
+    "ratio", [0, -1, -10],
+)
+def test_error_value_ratio(ratio):
+    with pytest.raises(ValueError):
+        recipe1 = Recipe("Шарлотка", [Ingredient("Соль", 2, "кг")])
+        recipe2 = recipe1.scale(ratio)
+
+def test_correct_len():
+    recipe1 = Recipe("Шарлотка", [Ingredient("Соль", 2, "кг"), Ingredient("Колбаса", 2, "кг"), Ingredient("Соль", 4, "кг"), Ingredient("Колбаса", 2, "г")])
+    assert len(recipe1) == 3
